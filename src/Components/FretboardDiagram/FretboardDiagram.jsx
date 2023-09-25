bc1bd1e761536d330709180cd1a4dd0c32a586e2
Matt Kirshman <mk@metalkindred.com>
2023-09-25 15:24:37 -0700

    added pdf download button

--- src/Components/FretboardDiagram/FretboardDiagram.jsx
+++ src/Components/FretboardDiagram/FretboardDiagram.jsx
@@ -1,51 +1,68 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useFretboard } from '../../Utilities/FretboardContext';
 import './FretboardDiagram.css';
+import jsPDF from 'jspdf';
 
 const FretboardDiagram = () => {
   // Use the useFretboard hook to access fret distances from the context
   const { fretDistances } = useFretboard();
-  const fromNutData = fretDistances.fretFromNutPlacements;
 
+  // Ensure that fretDistances and fretFromNutPlacements are defined before using them
+  const fromNutData = fretDistances?.fretFromNutPlacements || [];
+  const svgRef = useRef(null);
 
-  // Convert fret distances to a format suitable for rendering lines (e.g., pixels or other units)
-  // You'll need to calculate the appropriate scaling factor based on the measurement unit
-  // For this example, let's assume the fret distances are in millimeters
+  const svgWidth = 1500;
+  const scale = 96;
 
-  // Calculate a scaling factor (you can adjust this based on your needs)
+  // Use optional chaining to safely access the last element in the array
+  const svgHeight = fromNutData?.[fromNutData.length - 1] || 0;
 
+  const downloadPDF = () => {
+    // Create a new jsPDF instance
+    const doc = new jsPDF({
+      orientation: 'landscape',
+    });
 
-const svgWidth = 1500;
-const scale="96";
-const svgHeight = [fromNutData[fromNutData.length-1]].pop();
-  
+    // Get the SVG element
+    const svgElement = svgRef.current;
+
+    // Convert the SVG element to a data URL
+    const svgData = new XMLSerializer().serializeToString(svgElement);
+    const svgDataURL = `data:image/svg+xml;base64,${btoa(svgData)}`;
+
+    // Add the SVG to the PDF
+    doc.addImage(svgDataURL, 'JPEG', 10, 10, 280, 150);
+
+    // Download the PDF
+    doc.save('fretboard_diagram.pdf');
+  };
 
   return (
     <div className="fretboard-diagram">
-
-<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" width="200" height={(svgHeight*scale)+100}>
-  
-
-  <rect width="100%" height={svgHeight} fill="white" />
-
-
-  <line x1="0" y1="0" x2={svgWidth} y2="0" stroke="black" stroke-width="2" />
-
-
-  {fromNutData &&
-    fromNutData.map((element, index) => (
-      <line
-        key={index}
-        className="fret-line"
-        x1="0" // Adjust this value for your desired horizontal offset
-        y1={element*scale}
-        x2={svgWidth} // Adjust this value for your desired horizontal offset
-        y2={element*scale}
-        stroke="gray" // You can adjust the color here
-        stroke-width="1" // You can adjust the line thickness here
-      />
-    ))}
-</svg>
+      <svg
+        version="1.1"
+        id="Layer_1"
+        xmlns="http://www.w3.org/2000/svg"
+        width="200"
+        height={(svgHeight * scale) + 100}
+        ref={svgRef}
+      >
+        <rect width="100%" height={svgHeight} fill="white" />
+        <line x1="0" y1="0" x2={svgWidth} y2="0" stroke="black" strokeWidth="2" />
+        {fromNutData.map((element, index) => (
+          <line
+            key={index}
+            className="fret-line"
+            x1="0" // Adjust this value for your desired horizontal offset
+            y1={element * scale}
+            x2={svgWidth} // Adjust this value for your desired horizontal offset
+            y2={element * scale}
+            stroke="gray" // You can adjust the color here
+            strokeWidth="1" // You can adjust the line thickness here
+          />
+        ))}
+      </svg>
+      <button onClick={downloadPDF}>Download PDF</button>
     </div>
   );
 };
