f941dec97557b97b81feb4cb5da8984f32ce6709
Matt Kirshman <mk@metalkindred.com>
2023-09-25 12:06:58 -0700

    Added component to draw SVG

--- src/Components/FretboardDiagram/FretboardDiagram.jsx
+++ src/Components/FretboardDiagram/FretboardDiagram.jsx
@@ -1,14 +1,49 @@
 import React from 'react';
-import './FretboardDiagram.css'; // Import your CSS file
+import { useFretboard } from '../../Utilities/FretboardContext';
+import './FretboardDiagram.css';
 
-const FretboardDiagram = ({ fretDistances }) => {
-  // Convert fret distances to a format suitable for rendering lines (e.g., pixels or other units)
-  // You'll need to calculate the appropriate scaling factor based on the measurement unit
+const FretboardDiagram = () => {
+  const { fretDistances } = useFretboard(); // Assuming fretDistances is an array or object with numeric keys
+  const fromNutData = Object.keys(fretDistances || {}); // Use empty object as fallback
 
+  const scale = 1; // Adjust this value to control the scale of the SVG
+
+  // Calculate the width and height of the SVG based on the number of frets and the scale
+  const svgHeight = fromNutData.length * scale;
+  const svgWidth = 500; // Adjust the height as needed
 
   return (
     <div className="fretboard-diagram">
-     
+      <ul>
+        {fromNutData.map((element, index) => (
+          <li
+            key={index}
+            className="fret-line"
+            style={{
+              height: `${element * scale}px`, // Adjust the styling as needed
+            }}
+          >
+            {element}
+          </li>
+        ))}
+      </ul>
+      <svg width={svgWidth} height={svgHeight} xmlns="http://www.w3.org/2000/svg">
+        {/* Draw a line at 0 */}
+        <line x1="0" y1={svgHeight / 2} x2={svgWidth} y2={svgHeight / 2} stroke="black" strokeWidth="2" />
+
+        {/* Draw fret placements */}
+        {fromNutData.map((placement, index) => (
+          <line
+            key={index}
+            x1={placement * scale}
+            y1="0"
+            x2={placement * scale}
+            y2={svgHeight}
+            stroke="black"
+            strokeWidth="2"
+          />
+        ))}
+      </svg>
     </div>
   );
 };
